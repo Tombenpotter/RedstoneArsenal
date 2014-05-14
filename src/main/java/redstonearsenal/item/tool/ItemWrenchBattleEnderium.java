ab33c3de57d1cd6a698467cf4f4e323e9dc77a36
Tombenpotter <thomas@benguigui.fr>
2014-05-14 11:56:55 +0200

    Finished the battlewrench power.
    Yaaaaaaaaay

--- src/main/java/redstonearsenal/item/tool/ItemWrenchBattleEnderium.java
+++ src/main/java/redstonearsenal/item/tool/ItemWrenchBattleEnderium.java
@@ -1,6 +1,7 @@
 package redstonearsenal.item.tool;
 
 import java.util.Iterator;
+import java.util.Random;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,6 +12,7 @@ import redstonearsenal.util.Utils;
 
 public class ItemWrenchBattleEnderium extends ItemWrenchBattleRF {
 	int radius = 2;
+	Random random = new Random();
 
 	public ItemWrenchBattleEnderium(ToolMaterial toolMaterial) {
 		super(toolMaterial);
@@ -20,12 +22,19 @@ public class ItemWrenchBattleEnderium extends ItemWrenchBattleRF {
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		if (isEmpowered(stack))
+			radius = 4;
+
 		AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(player.posX - radius, player.posY - radius, player.posZ - radius, player.posX + radius, player.posY + radius, player.posZ + radius);
 		Iterator iter = world.getEntitiesWithinAABB(EntityLivingBase.class, bb).iterator();
 		if (iter != null) {
 			while (iter.hasNext()) {
 				EntityLivingBase entity = (EntityLivingBase) iter.next();
 				entity.attackEntityFrom(Utils.causePlayerFluxDamage(player), 2);
+				player.setAngles(-90, 90);
+				world.spawnParticle("largeexplode", player.posX, player.posY, player.posZ, 1, 1, 1);
+				if (random.nextInt(20) == 0 && !player.capabilities.isCreativeMode)
+					useEnergy(stack, true);
 			}
 		}
 		return stack;
