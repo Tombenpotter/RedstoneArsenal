d44c33191fead520b5d52f43cf3edcd98677051e
Tombenpotter <thomas@benguigui.fr>
2014-05-16 20:02:06 +0200

    Fixed the battlewrench not draining
    And I'm a derp. The boolean in useEnergy isn't to make it drain energy,
    but to simulate. Soo, setting it to true didn't drain energy. Hehe

--- src/main/java/redstonearsenal/item/tool/ItemWrenchBattleEnderium.java
+++ src/main/java/redstonearsenal/item/tool/ItemWrenchBattleEnderium.java
@@ -6,6 +6,8 @@ import java.util.Random;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import redstonearsenal.util.Utils;
@@ -13,6 +15,8 @@ import redstonearsenal.util.Utils;
 public class ItemWrenchBattleEnderium extends ItemWrenchBattleRF {
 	int radius = 2;
 	Random random = new Random();
+	int spinDamage = 2;
+	int resistanceEffect = 2;
 
 	public ItemWrenchBattleEnderium(ToolMaterial toolMaterial) {
 		super(toolMaterial);
@@ -25,21 +29,25 @@ public class ItemWrenchBattleEnderium extends ItemWrenchBattleRF {
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
-		if (isEmpowered(stack))
+		if (isEmpowered(stack)) {
 			radius = 4;
+			spinDamage = 4;
+			resistanceEffect = 4;
+		}
 
 		AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(player.posX - radius, player.posY - radius, player.posZ - radius, player.posX + radius, player.posY + radius, player.posZ + radius);
 		Iterator iter = world.getEntitiesWithinAABB(EntityLivingBase.class, bb).iterator();
 		if (iter != null) {
 			while (iter.hasNext()) {
 				EntityLivingBase entity = (EntityLivingBase) iter.next();
-				entity.attackEntityFrom(Utils.causePlayerFluxDamage(player), 2);
+				entity.attackEntityFrom(Utils.causePlayerFluxDamage(player), spinDamage);
 				player.setAngles(-90, 90);
 				world.spawnParticle("largeexplode", player.posX, player.posY, player.posZ, 1, 1, 1);
-				if (random.nextInt(20) == 0 && !player.capabilities.isCreativeMode)
-					useEnergy(stack, true);
+				if (!player.capabilities.isCreativeMode && random.nextInt(10) == 0)
+					useEnergy(stack, false);
 			}
 		}
+		player.addPotionEffect(new PotionEffect(Potion.resistance.id, 20, resistanceEffect, false));
 		return stack;
 	}
 }
